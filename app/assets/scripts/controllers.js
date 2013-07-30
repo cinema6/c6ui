7d1d1f9fbb3b092950434d79f8003fc7ddd4f428
Josh Minzner <jminzner@cinema6.com>
2013-07-30 14:54:54 +0000

    Volume features implemented.

--- app/assets/scripts/controllers.js
+++ app/assets/scripts/controllers.js
@@ -24,6 +24,13 @@
 							totalTime = event.target.duration;
 
 						self.ControlsController.progress((currentTime / totalTime) * 100);
+					})
+					.on('volumechange', function(event) {
+						var volumePercent = event.target.volume * 100,
+							muted = event.target.muted;
+
+						self.ControlsController.volumeChange(volumePercent);
+						self.ControlsController.muteChange(muted);
 					});
 			});
 
@@ -39,6 +46,18 @@
 
 			this.seek = function(percent) {
 				video.player.currentTime = (percent * video.player.duration) / 100;
+			};
+
+			this.volumeSeek = function(percent) {
+				video.player.volume = percent / 100;
+			};
+
+			this.mute = function() {
+				video.player.muted = true;
+			};
+
+			this.unmute = function() {
+				video.player.muted = false;
 			};
 
 			$scope.VideoPlayerCtrl = this;
