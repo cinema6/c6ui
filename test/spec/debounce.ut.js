2ea8db73bea8719a1d74a79c3b6f449731149a44
Josh Minzner <jminzner@cinema6.com>
2013-09-10 19:56:45 +0000

    Sped up unit tests for c6Debounce.

--- test/spec/debounce.ut.js
+++ test/spec/debounce.ut.js
@@ -4,18 +4,15 @@
 	define(['debounce/debounce'], function() {
 		describe('c6Debounce', function() {
 			var c6Debounce,
-				$window;
+				$window,
+				$timeout;
 
 			beforeEach(function() {
 				module('c6.ui');
-				module(function($provide) {
-					$provide.factory('$timeout', function($window) {
-						return $window.setTimeout;
-					});
-				});
-				inject(function(_c6Debounce_, _$window_) {
+				inject(function(_c6Debounce_, _$window_, _$timeout_) {
 					c6Debounce = _c6Debounce_;
 					$window = _$window_;
+					$timeout = _$timeout_;
 				});
 			});
 
@@ -29,21 +26,13 @@
 					flag = false,
 					debouncedSpy = c6Debounce(spy, 1000);
 
-				var interval = $window.setInterval(function() {
+				for (var i = 0; i < 300; i++) {
 					debouncedSpy();
+				}
 
-					if (count++ === 300) {
-						$window.clearInterval(interval);
-						flag = true;
-					}
-				}, 1);
+				$timeout.flush();
 
-				waitsFor(function() {
-					return flag;
-				});
-				runs(function() {
-					expect(spy.callCount).toBeLessThan(250);
-				});
+				expect(spy.callCount).toBe(1);
 			});
 
 			it('should call a function multiple times (but not every time)', function() {
@@ -52,48 +41,15 @@
 					flag = false,
 					debouncedSpy = c6Debounce(spy, 50);
 
-				var interval = $window.setInterval(function() {
-					debouncedSpy();
-
-					if (count++ === 300) {
-						$window.clearInterval(interval);
-						flag = true;
+				for (var i = 0; i < 25; i++) {
+					for (var j = 0; j < 100; j++) {
+						debouncedSpy();
 					}
-				}, 1);
+					$timeout.flush();
+				}
 
-				waitsFor(function() {
-					return flag;
-				});
-				runs(function() {
-					expect(spy.callCount).toBeGreaterThan(15);
-					expect(spy.callCount).toBeLessThan(250);
-				});
-			});
 
-			it('should work with multiple functions at the same time', function() {
-				var spy1 = jasmine.createSpy(),
-					spy2 = jasmine.createSpy(),
-					count = 0,
-					flag = false,
-					debouncedSpy1 = c6Debounce(spy1, 50),
-					debouncedSpy2 = c6Debounce(spy2, 100);
-
-				var interval = $window.setInterval(function() {
-					debouncedSpy1();
-					debouncedSpy2();
-
-					if(count++ === 300) {
-						$window.clearInterval(interval);
-						flag = true;
-					}
-				}, 1);
-
-				waitsFor(function() {
-					return flag;
-				});
-				runs(function() {
-					expect(spy1.callCount).toBeGreaterThan(spy2.callCount);
-				});
+				expect(spy.callCount).toBe(25);
 			});
 
 			it('should pass along the arguments', function() {
@@ -103,21 +59,11 @@
 					debouncedSpy = c6Debounce(spy, 10),
 					args = ['hello', 'foo'];
 
-				var interval = $window.setInterval(function() {
-					debouncedSpy.apply(this, args);
+				debouncedSpy.apply(this, args);
 
-					if (count++ === 50) {
-						$window.clearInterval(interval);
-						flag = true;
-					}
-				}, 1);
+				$timeout.flush();
 
-				waitsFor(function() {
-					return flag;
-				});
-				runs(function() {
-					expect(spy).toHaveBeenCalledWith(args);
-				});
+				expect(spy).toHaveBeenCalledWith(args);
 			});
 		});
 	});
