9407e0e4f24cf189aa8c7addd05a7c525db59a30
howard <howard.engelhart@outlook.com>
2013-10-07 17:54:19 -0400

    panels duration can be set via directive or anicache. added data storage to anicache

--- test/spec/panels.ut.js
+++ test/spec/panels.ut.js
@@ -6,50 +6,73 @@
             $rootScope,
             $timeout,
             $scope,
-            $animator,
             $log,
-            element,
-            upperPanel,
-            lowerPanel;
+            $element,
+            c6AniCache;
 
         describe('c6Panels',function(){
             var c6Panels;
             beforeEach(module('c6.ui'));
 
             beforeEach(function(){
-                inject(function(_$compile_,_$rootScope_,_$timeout_,_$log_,_$animator_){
+                inject(function(_$compile_,_$rootScope_,_$timeout_,_$log_,_c6AniCache_){
                     $compile        = _$compile_;
                     $rootScope      = _$rootScope_;
                     $timeout        = _$timeout_;
                     $log            = _$log_;
-                    $animator        = _$animator_;
-
-                    $scope      = $rootScope.$new();
-                    element = angular.element('<div c6-panels x-show="showPanels"></div>');
-                    $compile(element)($scope);
-                    $scope.$digest();
-                    upperPanel = angular.element(element.find('div')[0]);
-                    lowerPanel = angular.element(element.find('div')[1]);
+                    c6AniCache      = _c6AniCache_;
                 });
             });
 
-            it('should add stuff to the dom', function(){
-                var kids = element.find('div');
-                expect(kids.length).toEqual(2);
-            });
-/*
-            it('should be able to go up', function(){
-                console.log(angular.mock.dump(element));
-                $scope.$on('panelsDown',function(){
-                    console.log('HELLO');
+            describe('initialization',function(){
+                describe('static',function(){
+                    it('should use default with no duration',function(){
+                        $element = $compile(
+                           '<div c6-panels x-show="showPanels"></div>' 
+                        )($rootScope.$new());
+                        expect(c6AniCache.data('c6Panels').duration).toEqual(0.6);
+                    });
+                    it('should use a valid duration',function(){
+                        $element = $compile(
+                           '<div c6-panels x-duration="4" x-show="showPanels"></div>' 
+                        )($rootScope.$new());
+                        expect(c6AniCache.data('c6Panels').duration).toEqual(4);
+                    });
+                    it('should except if duration is invalid',function(){
+                        expect(function(){
+                            $element = $compile(
+                               '<div c6-panels x-duration="apple" x-show="showPanels"></div>' 
+                            )($rootScope.$new());
+                        }).toThrow('Invalid duration: apple. c6Panels duration must be a number.');
+                    });
                 });
-                $scope.showPanels = true;
-                $scope.$digest();
-                console.log(angular.mock.dump(element));
-                //var a = $animator($scope, { ngAnimate : '{show : \'upperPanel\' }' });
-                //a.show(upperPanel);
+                describe('dynamic',function(){
+                    beforeEach(function(){
+                        $element = $compile(
+                           '<div c6-panels x-duration="{{myDuration}}" x-show="showPanels"></div>' 
+                        )($rootScope.$new());
+                        $scope = $element.scope();
+                        $scope.$digest();
+                    });
+
+                    it('should set the default duration into the anicache', function(){
+                        expect(c6AniCache.data('c6Panels').duration).toEqual(0.6);
+                    });
+
+                    it('should handle changes to the duration', function(){
+                        $scope.myDuration = 2;
+                        $scope.$digest();
+                        expect(c6AniCache.data('c6Panels').duration).toEqual(2);
+                    });
+
+                    it('should except if duration is invalid', function(){
+                        expect(function(){
+                            $scope.myDuration = 'apple';
+                            $scope.$digest();
+                        }).toThrow('Invalid duration: apple. c6Panels duration must be a number.');
+                    });
+                });
             });
-*/
         });
     });
 }());
