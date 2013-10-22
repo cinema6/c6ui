11590322df17f4470d5d7d784759bc3d87de26da
Joshua Minzner <josh@minzner.org>
2013-10-22 17:01:36 -0400

    started adding docs.

--- src/c6ui.js
+++ src/c6ui.js
@@ -1,8 +1,24 @@
 (function(){
     'use strict';
+    /**
+     * C6UI Module
+     *
+     * @module c6ui
+     **/
     angular.module('c6.ui',[])
+        /**
+         * This service contains useful, general-purpose utility functions.
+         *
+         * @module c6ui
+         * @class c6ui
+         **/
         .service('c6ui', [function() {
             this.array = {
+                /**
+                 * Return the last item of an array.
+                 *
+                 * @method array.lastItem
+                 **/
                 lastItem: function(array) {
                     return array[array.length - 1];
                 }
